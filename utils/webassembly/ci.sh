yonihemi/swift

Fix local test runs by passing real test dir path

utils/webassembly/ci.sh
@@ -30,9 +30,13 @@ $BUILD_SCRIPT $FLAGS
 if [[ "$(uname)" == "Darwin" ]]; then
   # workaround: host target test directory is necessary to use run-test
   mkdir -p $BUILD_DIR/swift-macosx-x86_64/test-macosx-x86_64
+  HOST_PLATFORM=macosx
+else
+  HOST_PLATFORM=linux
 fi
 
-$RUN_TEST_BIN --build-dir $BUILD_DIR --target wasi-wasm32 test/stdlib/
+$RUN_TEST_BIN --build-dir $BUILD_DIR --target wasi-wasm32 \
+  $BUILD_DIR/swift-${HOST_PLATFORM}-x86_64/test-wasi-wasm32/stdlib
 
 if [[ "$(uname)" == "Linux" ]]; then
   echo "Skip running test suites for Linux"

Guang1234567/swift_wasm

utils/webassembly/ci.sh
@@ -30,14 +30,18 @@ $BUILD_SCRIPT $FLAGS
 if [[ "$(uname)" == "Darwin" ]]; then
   # workaround: host target test directory is necessary to use run-test
   mkdir -p $TARGET_BUILD_DIR/swift-macosx-x86_64/test-macosx-x86_64
+  HOST_PLATFORM=macosx
+else
+  HOST_PLATFORM=linux
 fi
 
 if [[ "$(uname)" == "Linux" ]]; then
-  $RUN_TEST_BIN --build-dir $TARGET_BUILD_DIR --target wasi-wasm32 test/stdlib/
+  $RUN_TEST_BIN --build-dir $TARGET_BUILD_DIR --target wasi-wasm32 \
+    $TARGET_BUILD_DIR/swift-${HOST_PLATFORM}-x86_64/test-wasi-wasm32/stdlib
   echo "Skip running test suites for Linux"
 else
-
-  $RUN_TEST_BIN --build-dir $TARGET_BUILD_DIR --target wasi-wasm32 test/stdlib/
+  $RUN_TEST_BIN --build-dir $TARGET_BUILD_DIR --target wasi-wasm32 \
+ 	$TARGET_BUILD_DIR/swift-${HOST_PLATFORM}-x86_64/test-wasi-wasm32/stdlib
 
   # Run test but ignore failure temporarily
   $BUILD_SCRIPT $FLAGS -t || true